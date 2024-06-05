ae0d77981ffe09aaa284a07d88fe6d2e91d04688
Gaikwad Radhika <139673080+Radhika-Gaikwad@users.noreply.github.com>
2024-06-05 12:34:09 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import RandomColor from './components/random-color'
+
+function App() {
+  
+
+  return (
+    <>
+      {/* {
+        Random clor components
+      } */}
+      <RandomColor/>
+    </>
+  )
+}
+
+export default App
